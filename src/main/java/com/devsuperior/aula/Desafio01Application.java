ae9970527a255438ac9c73d3dfc69ffbe50f1e43
Felipe Wendell <dev.felipelima@hotmail.com>
2024-07-30 07:49:24 -0300

    desafio1

---
+++ src/main/java/com/devsuperior/aula/Desafio01Application.java
@@ -0,0 +1,26 @@
+package com.devsuperior.aula;
+
+import com.devsuperior.aula.entities.Order;
+import com.devsuperior.aula.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio01Application implements CommandLineRunner {
+
+	@Autowired
+	private OrderService orderService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Order order = new Order(202,150.00,10.00);
+		System.out.println("Order code : " + order.getCode());
+		System.out.printf("Total value : %.2f%n ",orderService.total(order));
+	}
+}
---
+++ src/main/java/com/devsuperior/aula/services/OrderService.java
@@ -0,0 +1,19 @@
+package com.devsuperior.aula.services;
+
+import com.devsuperior.aula.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+    @Service
+    public class OrderService {
+
+        @Autowired
+        private ShippingService shippingService;
+
+        public double total(Order order) {
+            double discount = order.getBasic() * (order.getDiscount() / 100);
+            return (order.getBasic() - discount) + shippingService.shipment(order);
+        }
+
+    }
